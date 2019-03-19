9f98b0d5f1cffdbb0428ea3b14dd98683d006327
Vitalij21 <41307680+Vitalij21@users.noreply.github.com>
2019-03-19 23:01:42 +0300

    my-advanced-zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,49 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
+  //var number = 15;
+  //console.log(number);
+  //var ind = 0;
+  //var z = 0;
+  //var arr = [];
+  //var arr2 = [];
+  var b = Infinity;
+  
+    //for (var i=1; i<=number; i++) {
+    //arr.push(i);
+  for (var i=2; i<=base; i++) {
+    if (base % i == 0) {
+      var k = 0;
+        while (base % i == 0) {
+          k = k+1;
+          base = base / i;
+
+        }
+          var n = number;
+          var a = 0;
+          while (n / i > 0) {
+            n = Math.floor(n / i);            
+            a = a + n;
+
+          }     
+         
+          var b = Math.min(a / k, b);
+          var c = Math.floor(b);      
+
+    }
+
+  }
+    //var d = i;
+    //if (arr[i-1] % 5 == 0) {
+      //arr2.push(arr[i-1]);
+    //}
+  
+  //console.log (arr);
+  //console.log (arr2);
+  //a = arr2[i];
+
+ //while (d % 5 == 0) {
+    //d= d/5;
+    //z= z+1;
+  //}
+  return (c);
 }
